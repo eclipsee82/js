6ce1342a59ddb6ad44967cba1331718da61f90c3
Ramil Yalkunov <atlas9724@gmail.com>
2022-10-01 00:17:32 +0600

    constructor

--- index.js
+++ index.js
@@ -1,52 +1,5 @@
-var slider1 = sliderFactory.createNewSlider();
-var slider2 = sliderFactory.createNewSlider();
+var slider1 = new Slider();
+var slider2 = new Slider();
 
 slider1.start('slider1');
 slider2.start('slider2');
-
- 
-
-
-
-// var imagesUrls = [];
-// var currentImageIndex = 0;
-// var prevBtn = document.getElementById('show-prev');
-// var nextBtn = document.getElementById('show-next');
-// var slideImage = document.getElementById('slide-img');
-
-
-// prevBtn.addEventListener('click', onShowPrevBtnClick);
-// nextBtn.addEventListener('click', onShowNextBtnClick);
-
-
-// imagesUrls.push('https://www.kverner.ru/wp-content/uploads/2020/02/luchii-javascript-kursi.png');
-// imagesUrls.push('https://miro.medium.com/1*79n12W8P4jqfQhHD4_Rd1A.jpeg');
-// imagesUrls.push('https://miro.medium.com/max/480/1*VKY-Ldkt-iHobItql7G_5w.png');
-
-
-// slideImage.src = imagesUrls[currentImageIndex]
-
-// prevBtn.disabled = true;
-
-
-
-
-// function onShowPrevBtnClick(e) {
-//     currentImageIndex--;
-//     slideImage.src = imagesUrls[currentImageIndex]
-//     nextBtn.disabled = false;
-
-//     if (currentImageIndex === 0) {
-//         prevBtn.disabled = true;
-//     }
-// }
-
-// function onShowNextBtnClick(e) {
-//     currentImageIndex++;
-//     slideImage.src = imagesUrls[currentImageIndex]
-//     prevBtn.disabled = false;
-
-//     if (currentImageIndex === (imagesUrls.length - 1)) {
-//         nextBtn.disabled = true;
-//     }
-// }
--- slider.js
+++ slider.js
@@ -1,66 +1,58 @@
-var sliderFactory = {
-    createNewSlider: function() {
-        var newSlider = {
-            imagesUrls: [],
-            currentImageIndex: 0,
-            prevBtn: null,
-            nextBtn: null,
-            slideImage: null,
-        
-        
-            start: function (elId) {
-                var that = this;
-        
-                var elSelector = '#' + elId;
-                var el = document.querySelector(elSelector)
-        
-                this.prevBtn = el.querySelector('.show-prev');
-                this.nextBtn = el.querySelector('.show-next');
-                this.slideImage = el.querySelector('.slide-img');
-        
-                this.prevBtn.addEventListener('click', function (e) {
-                    that.onShowPrevBtnClick(e);
-                });
-        
-                this.nextBtn.addEventListener('click', function (e) {
-                    that.onShowNextBtnClick(e);
-                });
-        
-        
-                this.imagesUrls.push('https://www.kverner.ru/wp-content/uploads/2020/02/luchii-javascript-kursi.png');
-                this.imagesUrls.push('https://miro.medium.com/1*79n12W8P4jqfQhHD4_Rd1A.jpeg');
-                this.imagesUrls.push('https://miro.medium.com/max/480/1*VKY-Ldkt-iHobItql7G_5w.png');
-        
-        
-                this.slideImage.src = this.imagesUrls[this.currentImageIndex];
-                this.prevBtn.disabled = true;
-            },
-        
-        
-            onShowPrevBtnClick: function (e) {
-                this.currentImageIndex--;
-                this.slideImage.src = this.imagesUrls[this.currentImageIndex];
-                this.nextBtn.disabled = false;
-        
-                if (this.currentImageIndex === 0) {
-                    this.prevBtn.disabled = true;
-                }
-            },
-        
-            onShowNextBtnClick: function (e) {
-                this.currentImageIndex++;
-                this.slideImage.src = this.imagesUrls[this.currentImageIndex];
-                this.prevBtn.disabled = false;
-        
-                if (this.currentImageIndex === (this.imagesUrls.length - 1)) {
-                    this.nextBtn.disabled = true;
-                }
-            }
-        
-        
-        };
+function Slider(){
+    this.imagesUrls = [];
+    this.currentImageIndex = 0;
+    this.prevBtn = null;
+    this.nextBtn = null;
+    this.slideImage = null;
     
-        return newSlider;
-    }
+    this.start = function (elId) {
+        var that = this;
+
+        var elSelector = '#' + elId;
+        var el = document.querySelector(elSelector)
+
+        this.prevBtn = el.querySelector('.show-prev');
+        this.nextBtn = el.querySelector('.show-next');
+        this.slideImage = el.querySelector('.slide-img');
+
+        this.prevBtn.addEventListener('click', function (e) {
+            that.onShowPrevBtnClick(e);
+        });
+
+        this.nextBtn.addEventListener('click', function (e) {
+            that.onShowNextBtnClick(e);
+        });
+
+
+        this.imagesUrls.push('https://www.kverner.ru/wp-content/uploads/2020/02/luchii-javascript-kursi.png');
+        this.imagesUrls.push('https://miro.medium.com/1*79n12W8P4jqfQhHD4_Rd1A.jpeg');
+        this.imagesUrls.push('https://miro.medium.com/max/480/1*VKY-Ldkt-iHobItql7G_5w.png');
+
+
+        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+        this.prevBtn.disabled = true;
+    };
+
+    this.onShowPrevBtnClick = function (e) {
+        this.currentImageIndex--;
+        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+        this.nextBtn.disabled = false;
+
+        if (this.currentImageIndex === 0) {
+            this.prevBtn.disabled = true;
+        }
+    };
+
+    this.onShowNextBtnClick = function (e) {
+        this.currentImageIndex++;
+        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+        this.prevBtn.disabled = false;
+
+        if (this.currentImageIndex === (this.imagesUrls.length - 1)) {
+            this.nextBtn.disabled = true;
+        }
+    };
 };
+
+
 
